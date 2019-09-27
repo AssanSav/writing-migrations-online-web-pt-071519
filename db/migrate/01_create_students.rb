1544790052712647d1bc259c1ed2b7396af1242e
assane savadogo <assane.savadogo81@gmail.com>
2019-09-27 21:26:18 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+<<<<<<< HEAD
     
     def change 
         create_table :students do |table|
@@ -6,4 +7,13 @@ class CreateStudents < ActiveRecord::Migration[5.1]
         end
     end
     
+=======
+  
+  def change 
+    create_table :students do |table|
+      table.string :name
+    end
+  end
+  
+>>>>>>> 64a3a94388f9257a5dd5032f2a365f44c4199e8c
 end
--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,15 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
+<<<<<<< HEAD
     def change 
       add_column :students, :grade, :integer 
       add_column :students, :birthdate, :string
     end
   end
+=======
+  def change 
+    add_column :grade, :integer 
+    add_column :birthdate, :string
+  end
+end
+>>>>>>> 64a3a94388f9257a5dd5032f2a365f44c4199e8c
